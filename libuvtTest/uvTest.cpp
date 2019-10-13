07521274b01417c008dbc707b06e167a1795c200
siweilxy <siweilxy@163.com>
2019-10-13 21:40:06 +0800

    add uvTest

---
+++ libuvtTest/uvTest.cpp
@@ -0,0 +1,24 @@
+/*
+ * recv.cpp
+ *
+ *  Created on: Sep 4, 2019
+ *      Author: siwei
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <strings.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(int argc, char* argv[])
+{
+
+    printf("uvtestStart\n");
+
+
+    return 0;
+}
